9f4cf5fe67b315adb1c8afe96bbc98b7df3a8601
Rajdeep <113767107+rajdeepk012@users.noreply.github.com>
2025-06-11 13:12:18 +0530

    Update CaseTrackerRepository to use HistoryData

--- src/main/java/co/mw/gf_dashboard_service/service/CaseTrackerRepository.java
+++ src/main/java/co/mw/gf_dashboard_service/service/CaseTrackerRepository.java
@@ -1,15 +1,17 @@
 package co.mw.gf_dashboard_service.service;
 
-import co.mw.gf_dashboard_service.model.Case;
-
-import org.bson.Document;
+import co.mw.gf_dashboard_service.model.HistoryData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-
+/**
+ * Repository for accessing history data for the case tracker.
+ * <p>
+ * The {@link HistoryData} model is mapped to the {@code history_data}
+ * collection in MongoDB via the {@code @Document} annotation. Extending
+ * {@link MongoRepository} provides basic CRUD operations which can be used by
+ * the service layer.
+ */
 @Repository
-public interface CaseTrackerRepository extends MongoRepository<Case, String> {
-
+public interface CaseTrackerRepository extends MongoRepository<HistoryData, String> {
 }
